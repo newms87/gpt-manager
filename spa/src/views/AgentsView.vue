2e30c06d0afe113f051b42c012a2d185b37256dd
Daniel Newman <dan@flytedesk.com>
2024-04-27 15:44:48 -0300

    AgentsView single component

--- spa/src/views/AgentsView.vue
+++ spa/src/views/AgentsView.vue
@@ -1,41 +1,26 @@
 <template>
-    <ActionTableLayout>
-        <template #toolbar>
-            <ActionToolbar
-                refresh
-                :actions="filterActions({ batch: true })"
-                :action-target="selectedRows"
-                :exporter="() => Agents.download(filter)"
-                @refresh="refreshAll"
-            />
-        </template>
-        <template #filters>
-            <CollapsableFiltersSidebar
-                v-model:filter="filter"
-                show-filters
-                name="agents-sidebar"
-                :filter-fields="filterFields"
-            />
-        </template>
-        <ActionTable
-            v-model:selected-rows="selectedRows"
-            v-model:quasar-pagination="quasarPagination"
-            label="Agents"
-            name="agents"
-            class="bg-slate-600"
-            :summary="summary"
-            :is-loading-list="isLoadingList"
-            :is-loading-summary="isLoadingSummary"
-            :paged-items="pagedItems"
-            :columns="columns"
-        />
-        <PanelsDrawer
-            v-if="activeItem"
-            v-model="activePanel"
-            :panels="panels"
-            @close="activeItem = null"
-        />
-    </ActionTableLayout>
+    <ActionTableLayout
+        v-model:filter="filter"
+        v-model:selected-rows="selectedRows"
+        v-model:pagination="quasarPagination"
+        v-model:active-panel="activePanel"
+        v-model:active-item="activeItem"
+        label="Agents"
+        name="agents"
+        table-class="bg-slate-600"
+        show-filters
+        refresh-button
+        :filter-fields="filterFields"
+        :batch-actions="filterActions({ batch: true })"
+        :exporter="() => Agents.download(filter)"
+        :summary="summary"
+        :loading-list="isLoadingList"
+        :loading-summary="isLoadingSummary"
+        :paged-items="pagedItems"
+        :columns="columns"
+        :panels="panels"
+        @refresh="refreshAll"
+    />
 </template>
 <script setup>
 import { filterActions } from "@/components/Agents/agentsActions";
@@ -56,7 +41,7 @@ import {
 import { filterFields } from "@/components/Agents/agentsFilters";
 import { panels } from "@/components/Agents/agentsPanels";
 import { Agents } from "@/routes/agents";
-import { ActionTable, ActionTableLayout, ActionToolbar, CollapsableFiltersSidebar, PanelsDrawer } from "quasar-ui-danx";
+import { ActionTableLayout } from "quasar-ui-danx";
 
 initialize();
 </script>
