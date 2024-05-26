8555740166a61509c0df1876180ca95b455ca6e9
Daniel Newman <dan@flytedesk.com>
2024-05-25 21:29:27 -0300

    adding domains and SPA deployment in vapor build process

--- spa/deploy.sh
+++ spa/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-CF_DISTRIBUTION_ID=E7NP0I2WMQMYW
+./build.sh
 
-# Inject the commit SHA into the environment as the App Version
-# Used to both verify current running version and to invalidate cache when server version does not match
-VITE_APP_APP_VERSION=$(git rev-parse HEAD) yarn build-only
+CF_DISTRIBUTION_ID=E7NP0I2WMQMYW
 
 # Sync to S3 static website bucket and invalidate CloudFront cache for the index.html file
 aws s3 sync ./dist s3://ai.on-demands.com/ --delete --profile gpt
