025639108e1ab2c86e41a5d264ceaf2778b401ec
Daniel Newman <dan@flytedesk.com>
2024-09-03 15:42:01 -0600

    Action table create button attribute

--- spa/src/components/Modules/Prompts/Directives/PromptDirectiveTable.vue
+++ spa/src/components/Modules/Prompts/Directives/PromptDirectiveTable.vue
@@ -6,22 +6,12 @@
 		filter-class="bg-slate-500"
 		show-filters
 		refresh-button
-		:filters="filters"
-		:columns="columns"
-		:panels="panels"
-	>
-		<template #action-toolbar>
-			<QBtn class="bg-green-900 mr-4 px-4" @click="createAction.trigger()">Create</QBtn>
-		</template>
-	</ActionTableLayout>
+		create-button
+	/>
 </template>
 <script setup lang="ts">
-import { dxPromptDirective } from "@/components/Modules/Prompts/Directives/config";
-import { columns } from "@/components/Modules/Prompts/Directives/config/columns";
-import { filters } from "@/components/Modules/Prompts/Directives/config/filters";
-import { panels } from "@/components/Modules/Prompts/Directives/config/panels";
+import { dxPromptDirective } from "@/components/Modules/Prompts/Directives";
 import { ActionTableLayout } from "quasar-ui-danx";
 
 dxPromptDirective.initialize();
-const createAction = dxPromptDirective.getAction("create");
 </script>
--- spa/src/components/Modules/Prompts/Schemas/PromptSchemaTable.vue
+++ spa/src/components/Modules/Prompts/Schemas/PromptSchemaTable.vue
@@ -6,22 +6,12 @@
 		filter-class="bg-slate-500"
 		show-filters
 		refresh-button
-		:filters="filters"
-		:columns="columns"
-		:panels="panels"
-	>
-		<template #action-toolbar>
-			<QBtn class="bg-green-900 mr-4 px-4" @click="createAction.trigger()">Create</QBtn>
-		</template>
-	</ActionTableLayout>
+		create-button
+	/>
 </template>
 <script setup lang="ts">
 import { dxPromptSchema } from "@/components/Modules/Prompts/Schemas";
-import { columns } from "@/components/Modules/Prompts/Schemas/config/columns";
-import { filters } from "@/components/Modules/Prompts/Schemas/config/filters";
-import { panels } from "@/components/Modules/Prompts/Schemas/config/panels";
 import { ActionTableLayout } from "quasar-ui-danx";
 
 dxPromptSchema.initialize();
-const createAction = dxPromptSchema.getAction("create");
 </script>
