ac24985f65893562de305473ee3c1bc041fa3c78
Daniel Newman <dan@flytedesk.com>
2025-02-27 22:38:03 -0300

    Updating to use Convert API Bearer Token

--- config/convertapi.php
+++ config/convertapi.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'url'        => env('CONVERT_API_URL', 'https://v2.convertapi.com/convert/'),
-    'api_key'    => env('CONVERT_API_KEY'),
-    'secret_key' => env('CONVERT_API_SECRET'),
+    'url'       => env('CONVERT_API_URL', 'https://v2.convertapi.com/convert/'),
+    'api_token' => env('CONVERT_API_TOKEN'),
 ];
