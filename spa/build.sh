9de239768dc8e323594d7397bdf919fd91d45b39
Daniel Newman <dan@flytedesk.com>
2025-05-13 18:14:39 -0300

    Build > Update configs, dependencies, and build scripts
    • Update IDE PHP unit settings to include alternate configuration
    • Relocate clockwork dependency to dev and update composer.lock hashes
    • Adjust Docker queue command for multiple queue processing
    • Enhance SPA build script to conditionally build upstream UI and streamline build command
    • Add browser compatibility dependency in SPA package

--- spa/build.sh
+++ spa/build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+DANX_DIR="../../quasar-ui-danx"
+
+# If the quasar-ui-danx directory exists, build that first
+if [ -d "$DANX_DIR" ]; then
+  CURRENT_DIR=$(pwd)
+  cd ../../quasar-ui-danx/ui || exit
+  rm -rf node_modules
+  yarn
+  yarn build
+
+  cd "$CURRENT_DIR" || exit
+fi
+
+rm -rf node_modules
 yarn
 
 # Inject the commit SHA into the environment as the App Version
 # Used to both verify current running version and to invalidate cache when server version does not match
-VITE_APP_APP_VERSION=$(git rev-parse HEAD) yarn build-only
+VITE_APP_APP_VERSION=$(git rev-parse HEAD) yarn build
